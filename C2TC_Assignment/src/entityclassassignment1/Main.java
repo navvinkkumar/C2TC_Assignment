73362ba4e2f3a7bc7efc480698c96f12f8bdd239
navvin's acer <navvin's acer@navvin>
2025-07-22 13:52:48 +0530

    Assignment

---
+++ C2TC_Assignment/src/entityclassassignment1/Main.java
@@ -0,0 +1,12 @@
+package entityclassassignment1;
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Navvin", 20, "IT");
+        Student s2 = new Student("Anu", 21, "CSE");
+        Student s3 = new Student("Raj", 22, "ECE");
+
+        System.out.println("Student Details:");
+        s1.display();
+        s2.display();
+        s3.display();
+    }}
---
+++ C2TC_Assignment/src/entityclassassignment2/Product.java
@@ -0,0 +1,18 @@
+package entityclassassignment2;
+
+ class Product {
+	public String productName;
+    public int productId;
+    public double price;
+
+    public Product(String productName, int productId, double price) {
+        this.productName = productName;
+        this.productId = productId;
+        this.price = price;
+    }
+
+    public void display() 
+    {
+        System.out.println("Product ID: " + productId + ", Name: " + productName + ", Price: ₹" +price);
+    }
+}
