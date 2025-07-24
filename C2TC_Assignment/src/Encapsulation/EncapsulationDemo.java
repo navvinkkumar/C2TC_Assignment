2486107a69349e6445f455418d028b5e0c3cca68
navvin's acer <navvin's acer@navvin>
2025-07-24 12:05:47 +0530

    Assignment 2

---
+++ C2TC_Assignment/src/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Navin");
+		obj.setAge(20);
+		System.out.println(obj);
+	}
+}
